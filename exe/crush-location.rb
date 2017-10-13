a05fbf66d772d3c69b9d07e894b10aaf468499be
Stuart Harland <s.harland@livelinktechnology.net>
2017-10-13 13:26:56 +0100

    Fix

--- exe/crush-location.rb
+++ exe/crush-location.rb
@@ -1,3 +1,6 @@
-require 'ceph-crush-location'
+#!/bin/env ruby
+require 'ceph/crush/location'
 
 Ceph::Crush::Location::Options::Parser.parse
+
+puts Ceph::Crush::Location.options
--- lib/ceph/crush/location.rb
+++ lib/ceph/crush/location.rb
@@ -1,5 +1,7 @@
-require "ceph/crush/location/version"
-require "ceph/crush/location/options/parser"
+require 'rubygems'
+require 'active_support'
+require 'ceph/crush/location/version'
+require 'ceph/crush/location/options/parser'
 
 module Ceph
   module Crush
